a74b338d897bdc0d51be7a7a552566faa313555a
ishamo <sha_moon@126.com>
2016-04-28 21:13:08 +0800

    finish argsetup

--- argsetup.cpp
+++ argsetup.cpp
@@ -21,7 +21,11 @@ void Argsetup::on_pushButton_clicked()
 {
     //lookup arguments
     MyData Data;
-    *(quint32 *)(Data.Header+17) = quint32(0x0001);
+    Data.Header[17] = 0;
+    Data.Header[18] = 0;
+    Data.Header[19] = 0;
+    Data.Header[20] = 16;
+
     quint16 tmp = Data.crc(Data.Header+14, 9);
     Data.Header[23] = tmp >> 8;
     Data.Header[24] = (quint8)tmp;
@@ -68,10 +72,25 @@ void Argsetup::readyRead()
 
     qDebug() << Buffer;
 
-    ui->label_5->setText(QVariant(Buffer[24]).toString());
-    ui->label_6->setText(QVariant(Buffer[25]).toString());
-    ui->label_7->setText(QVariant(Buffer[26]).toString());
-    ui->label_8->setText(QVariant(Buffer[27]).toString());
-    ui->label_9->setText(QVariant(Buffer[28]).toString());
+    ui->label_9->setText(QVariant(Buffer[35]).toString()); //xindao
+
+    ui->label_5->setText(QVariant(Buffer[23]).toString());
+    ui->label_6->setText(QVariant(Buffer[24]).toString());
+    ui->label_7->setText(QVariant(Buffer[25]).toString());
+    ui->label_8->setText(QVariant(Buffer[26]).toString());
+
+    ui->label_19->setText(QVariant(Buffer[27]).toString());
+    ui->label_20->setText(QVariant(Buffer[28]).toString());
+    ui->label_21->setText(QVariant(Buffer[29]).toString());
+    ui->label_22->setText(QVariant(Buffer[30]).toString());
+
+    ui->label_12->setText(QVariant(Buffer[31]).toString());
+    ui->label_13->setText(QVariant(Buffer[32]).toString());
+    ui->label_14->setText(QVariant(Buffer[33]).toString());
+    ui->label_15->setText(QVariant(Buffer[34]).toString());
+
+
+  //  ui->label_9->setText(QVariant(Buffer[28]).toString());
+
     socket->close();
 }
